30c448c862bd8bdfab402f294df5d83e3ebd7dcc
christyuda <chrisyuda@ulbi.ac.id>
2024-06-07 21:58:53 +0700

    feat : pembuatan login dan daftar dan url endpoint

---
+++ js/login.js
@@ -0,0 +1,63 @@
+import { endpointLogin } from "../js/url.js";
+
+document.getElementById("signInButton").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const emailOrUsername = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const data = {
+    username: emailOrUsername,
+    password: password,
+  };
+
+  fetch(endpointLogin, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      if (response.status === 401) {
+        throw new Error("Password salah. Silakan coba lagi.");
+      } else {
+        throw new Error("Terjadi kesalahan pada server. Silakan coba lagi nanti.");
+      }
+    }
+    return response.json();
+  })
+  .then((data) => {
+    if (data.token) {
+      localStorage.setItem("LOGIN", data.token);
+      document.cookie = `LOGIN=${data.token};path=/;max-age=3600`;
+
+      // Tampilkan SweetAlert bahwa login berhasil
+      return Swal.fire({
+        icon: 'success',
+        title: 'Login Berhasil',
+        text: 'Selamat, Anda berhasil login!',
+        confirmButtonText: 'OK'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          if (data.user && data.user.id_role === 1) {
+            window.location.href = "dashboard-admin.html";
+          } else if (data.user && data.user.id_role === 2) {
+            window.location.href = "dashboard.html";
+          }
+        }
+      });
+    } else {
+      throw new Error("Token tidak diterima");
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    Swal.fire({
+      icon: "error",
+      title: "Error Login",
+      text: error.message,
+    });
+  });
+});
---
+++ js/url.js
@@ -0,0 +1,2 @@
+export const endpointLogin = "http://127.0.0.1:3000/login"
+export const endpointRegister = "http://127.0.0.1:3000/register"
